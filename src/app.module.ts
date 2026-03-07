08a9cce8f46359c35b888712e0556ff494abe295
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-03-07 23:07:03 +0400

    feature/prismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { PrismaModule } from './prisma/prisma.module';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [PrismaModule],
   providers: [AppService],
 })
 export class AppModule {}
