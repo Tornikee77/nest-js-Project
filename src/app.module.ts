c0dbb4f48e02132aea3bdde351cd585ff9746a2e
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-03-08 11:52:58 +0400

    feature/auth resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   providers: [AppService],
 })
 export class AppModule {}
