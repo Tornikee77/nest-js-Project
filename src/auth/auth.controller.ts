9e2de5c2127f1412e69b112ca0323ba3c4b0f662
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-03-10 23:38:12 +0400

    feature/JwtAuthGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,8 +2,9 @@ import { Body, Controller, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { AuthResponseDto } from './dto/authResponse.dto';
-import { RefreshTokenGuard } from './guards/refreshToken.guard';
+import { RefreshTokenGuard } from './guards/refresh-token.guard';
 import { GetUser } from 'src/common/decorators/get-user.decorator';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +17,11 @@ export class AuthController {
   @UseGuards(RefreshTokenGuard)
   async refresh(@GetUser('id') userId: string): Promise<AuthResponseDto> {
     return await this.authService.refreshTokens(userId);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  async logout(@GetUser('id') userId: string): Promise<{ message: string }> {
+    await this.authService.logout(userId);
+    return { message: 'Logged out successfully' };
   }
 }
