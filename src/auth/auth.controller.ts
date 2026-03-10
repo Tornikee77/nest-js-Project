edcd1fe4b190ea3f00c0f26f150111385270d7ab
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-03-10 23:56:41 +0400

    feature/login function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { AuthResponseDto } from './dto/authResponse.dto';
@@ -12,24 +19,29 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
+  @HttpCode(201)
   async register(@Body() registerDto: RegisterDto): Promise<AuthResponseDto> {
     return await this.authService.register(registerDto);
   }
 
   @Post('refresh')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(RefreshTokenGuard)
   async refresh(@GetUser('id') userId: string): Promise<AuthResponseDto> {
     return await this.authService.refreshTokens(userId);
   }
 
   @Post('logout')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(JwtAuthGuard)
   async logout(@GetUser('id') userId: string): Promise<{ message: string }> {
     await this.authService.logout(userId);
     return { message: 'Logged out successfully' };
   }
 
-  @Post("login")
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(@Body() loginDto: LoginDto): Promise<AuthResponseDto> {
     return await this.authService.login(loginDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { AuthResponseDto } from './dto/authResponse.dto';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { randomBytes } from 'crypto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -119,5 +120,27 @@ export class AuthService {
       where: { id: userId },
       data: { refreshToken: null },
     });
+  }
+
+  async login(loginDto: LoginDto): Promise<AuthResponseDto> {
+    const { email, password } = loginDto;
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (!user || (await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+    const tokens = await this.generateTokens(user.id, user.email);
+    await this.updateRefreshToken(user.id, tokens.refreshToken);
+    return {
+      ...tokens,
+      user: {
+        id: user.id,
+        email: user.email,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        role: user.role,
+      },
+    };
   }
 }
