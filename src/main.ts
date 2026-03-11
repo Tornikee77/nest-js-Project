11cdbde4f17d2a7806af612926116e840a0cddb2
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-03-11 18:49:29 +0400

    feature/global pipes and cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN?.split(',') || 'http://localhost:3000',
+    Credentials: true,
+    method: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((error) => {
