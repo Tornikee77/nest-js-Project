ddc6b2d04fcc0ca9feab49fef08dad3f8e3a388e
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-03-12 02:01:47 +0400

    Update authResponse.dto.ts

--- src/auth/dto/authResponse.dto.ts
+++ src/auth/dto/authResponse.dto.ts
@@ -1,28 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Role } from 'generated/prisma';
+import { Role } from '@prisma/client';
 
 export class AuthResponseDto {
   @ApiProperty({
-    description: 'The access token',
-    example: 'eyJhbGciOiJIUzI1Ni...',
+    description: 'The access token for authentication',
+    example:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c',
   })
   accessToken: string;
 
   @ApiProperty({
-    description: 'The refresh token',
-    example: 'eyJhbGciOiJIUzI1Ni...',
-  })
-  refreshToken: string;
-
-  @ApiProperty({
+    description: 'The refresh token for authentication',
     example: {
-      id: 'uuid-123',
-      email: 'user@example.com',
+      id: '1234567890',
+      email: '<Email>',
       firstName: 'John',
       lastName: 'Doe',
       role: 'USER',
     },
   })
+  refreshToken: string;
   user: {
     id: string;
     email: string;
