fcf155e71a85d5c34255cb5558ae8d81a87bdeff
Tornikee77 <tornikeesitashvili999@gmail.com>
2026-03-12 23:14:32 +0400

    feature/find all users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RoleGuard } from 'src/auth/guards/roles.guard';
+import { RequestWithUser } from 'src/common/interfaces/request-with-user.interface';
+import { UserResponseDto } from './dto/user-response.dto';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { Role } from '@prisma/client';
 
 @ApiTags('users')
 @ApiBearerAuth('JWT-auth')
@@ -11,16 +20,38 @@ import { RoleGuard } from 'src/auth/guards/roles.guard';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get("me")
+  @Get('me')
   @ApiOperation({
     summary: 'Get current user',
   })
   @ApiResponse({
-    status:200,
+    status: 200,
     description: 'Current user retrieved successfully',
     type: UserResponseDto,
   })
   @ApiResponse({
-    status:401, description:"Unauthorized"
+    status: 401,
+    description: 'Unauthorized',
   })
+  async getProfile(@Req() req: RequestWithUser): Promise<UserResponseDto> {
+    return await this.usersService.findOne(req.user.id);
+  }
+
+  @Get()
+  @Roles(Role.ADMIN)
+  @ApiOperation({
+    summary: 'Get all users',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'List of all users',
+    type: [UserResponseDto],
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  async findAll(): Promise<UserResponseDto[]> {
+    return await this.usersService.findAll();
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,9 +4,9 @@ import { UserResponseDto } from './dto/user-response.dto';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prismaService: PrismaService) {}
   async findOne(userId: string): Promise<UserResponseDto> {
-    const user = await this.prisma.user.findUnique({
+    const user = await this.prismaService.user.findUnique({
       where: { id: userId },
       select: {
         id: true,
@@ -23,5 +23,22 @@ export class UsersService {
     if (!user) {
       throw new NotFoundException('User not fount');
     }
+    return user;
+  }
+
+  async findAll(): Promise<UserResponseDto[]> {
+    const users = await this.prismaService.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        firstName: true,
+        lastName: true,
+        role: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+      orderBy: { createdAt: 'desc' },
+    });
+    return users;
   }
 }
